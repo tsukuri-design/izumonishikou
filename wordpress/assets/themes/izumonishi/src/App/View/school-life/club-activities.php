4645326ff873fd37d9320a5fb66bba8d18b5ef95
life15bp <t.mukaiyama.d@gmail.com>
2025-10-06 19:26:23 +0900

    update: current...

--- wordpress/assets/themes/izumonishi/src/App/View/school-life/club-activities.php
+++ wordpress/assets/themes/izumonishi/src/App/View/school-life/club-activities.php
@@ -13,14 +13,24 @@
 </div>
 <div class="singular__main">
     <?php /* if (getHeaderLinks(get_the_content()) !== ''): ?>
-   <div class="header_links inaction inaction_opacity"><?php echo getHeaderLinks(get_the_content()); ?></div>
+<div class="header_links inaction inaction_opacity"><?php echo getHeaderLinks(get_the_content()); ?></div>
 <?php endif; */ ?>
     <div class="club-activities">
 
         <div class="club-activities-first-view">
-            <figure class="club-activities-first-view-image"><?php echo picture_array(['src' => 'img/', 'name' => 'club-activities-first-view', 'alt' => 'club-activities-first-view']); ?></figure>
+            <figure class="club-activities-first-view-image"><?php echo picture_array([
+                'src' => 'img/school-life/club-activities/',
+                'name' => 'first-view',
+                'alt' => '部活動画像'
+            ]); ?></figure>
             <div class="club-activities-first-view-text">
-
+                <h2 class="club-activities-first-view-heading2">打ち込める場所が<br>きっと見つかる</h2>
+                <p class="club-activities-first-view-explain">
+                    仲間と切磋琢磨しながら心身を鍛え、<br>
+                    充実した学校生活を送ることができます。<br>
+                    運動部・文化部ともに多彩なクラブが活動しており、<br>
+                    一人ひとりの興味や個性を伸ばせる環境です。
+                </p>
             </div>
         </div>
     </div>
