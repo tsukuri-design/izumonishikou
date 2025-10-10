6ccaef35712e06cc74cb030774eebd8110a466f2
Taylor <t.harris.afd@gmail.com>
2025-10-10 10:52:15 +0900

    update: facilities hidden arrow

--- wordpress/assets/themes/izumonishi/src/App/View/school-life/facilities.php
+++ wordpress/assets/themes/izumonishi/src/App/View/school-life/facilities.php
@@ -112,7 +112,7 @@
             <div class="facility-slide facility-slide-11">
                 <?php echo picture('', 'school-life/facility/facility12', 'jpg', '', 'webp', '', '', '', ''); ?>
             </div>
-            <div class="arrow"><?php svg('slider_arrow'); ?></div>
+            <div class="arrow" style="display:none;"><?php svg('slider_arrow'); ?></div>
         </div>
         <div class="facility-slide-wrap facility-slide-wrap-13">
             <div class="slider-title">第二多目的室</div>
